4a3110babf4074694590552b0c1db2d6ee7f78d5
DanteDeGrave <vladimir.system@gmail.com>
2021-03-15 15:53:40 +0500

    Деструктурировал функцию отрисовки

--- js/card-generator.js
+++ js/card-generator.js
@@ -10,28 +10,64 @@ const generationCard = (cardsArray) => {
   const cardListFragment = document.createDocumentFragment();
   cardsArray.forEach((element) =>{
     const cardClone = card.cloneNode(true);
-    const popupTitle = cardClone.querySelector('.popup__title');
-    const popupTextAddress = cardClone.querySelector('.popup__text--address');
-    const popupTextPrice = cardClone.querySelector('.popup__text--price');
-    const popupType = cardClone.querySelector('.popup__type');
-    const popupTextCapacity = cardClone.querySelector('.popup__text--capacity');
-    const popupTextTime = cardClone.querySelector('.popup__text--time');
-    const popupFeaturesList = cardClone.querySelector('.popup__features');
-    const popupDescription = cardClone.querySelector('.popup__description');
-    const popupPhoto = cardClone.querySelector('.popup__photos');
-    element.offer.title ? popupTitle.textContent = element.offer.title : popupTitle.remove();
-    element.offer.address ? popupTextAddress.textContent = element.offer.address : popupTextAddress.remove();
-    element.offer.price ? popupTextPrice.textContent = `${element.offer.price} ₽/ночь` : popupTextPrice.remove();
-    element.offer.type ? popupType.textContent = TYPE_APARTMENTS[element.offer.type] : popupType.remove();
-    element.offer.rooms && element.offer.guests ? popupTextCapacity.textContent =
-      `${element.offer.rooms} комнаты для ${element.offer.guests} гостей` : popupTextCapacity.remove();
-    element.offer.checkin && element.offer.checkout ? popupTextTime.textContent =
-      `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}` : popupTextTime.remove();
-    element.offer.features.length > 0 ? renderFeatureList(popupFeaturesList, element.offer.features) : popupFeaturesList.remove();
-    element.offer.description ? popupDescription.textContent = element.offer.description : popupDescription.remove();
-    element.offer.photos.length > 0 ? renderPhoto(popupPhoto, element.offer.photos) : popupPhoto.remove();
+    fillTitle(cardClone, element);
+    fillAddress(cardClone, element);
+    fillPrice(cardClone, element);
+    fillType(cardClone, element);
+    fillRooms(cardClone, element);
+    fillCheck(cardClone, element);
+    fillFeatures(cardClone, element);
+    fillDescription(cardClone, element);
+    fillPhotos(cardClone, element);
     cardListFragment.appendChild(cardClone);
   });
+};
+
+const fillTitle = (clone, element) => {
+  const popupTitle = clone.querySelector('.popup__title');
+  element.offer.title ? popupTitle.textContent = element.offer.title : popupTitle.remove();
+};
+
+const fillAddress = (clone, element) => {
+  const popupTextAddress = clone.querySelector('.popup__text--address');
+  element.offer.address ? popupTextAddress.textContent = element.offer.address : popupTextAddress.remove();
+};
+
+const fillPrice = (clone, element) => {
+  const popupTextPrice = clone.querySelector('.popup__text--price');
+  element.offer.price ? popupTextPrice.textContent = `${element.offer.price} ₽/ночь` : popupTextPrice.remove();
+};
+
+const fillType = (clone, element) => {
+  const popupType = clone.querySelector('.popup__type');
+  element.offer.type ? popupType.textContent = TYPE_APARTMENTS[element.offer.type] : popupType.remove();
+};
+
+const fillRooms = (clone, element) => {
+  const popupTextCapacity = clone.querySelector('.popup__text--capacity');
+  element.offer.rooms && element.offer.guests ? popupTextCapacity.textContent =
+    `${element.offer.rooms} комнаты для ${element.offer.guests} гостей` : popupTextCapacity.remove();
+};
+
+const fillCheck = (clone, element) => {
+  const popupTextTime = clone.querySelector('.popup__text--time');
+  element.offer.checkin && element.offer.checkout ? popupTextTime.textContent =
+    `Заезд после ${element.offer.checkin}, выезд до ${element.offer.checkout}` : popupTextTime.remove();
+};
+
+const fillFeatures = (clone, element) => {
+  const popupFeaturesList = clone.querySelector('.popup__features');
+  element.offer.features.length > 0 ? renderFeatureList(popupFeaturesList, element.offer.features) : popupFeaturesList.remove();
+};
+
+const fillDescription = (clone, element) => {
+  const popupDescription = clone.querySelector('.popup__description');
+  element.offer.description ? popupDescription.textContent = element.offer.description : popupDescription.remove();
+}
+
+const fillPhotos = (clone, element) => {
+  const popupPhoto = clone.querySelector('.popup__photos');
+  element.offer.photos.length > 0 ? renderPhoto(popupPhoto, element.offer.photos) : popupPhoto.remove();
 };
 
 const renderFeatureList = (node, featuresArray) => {
